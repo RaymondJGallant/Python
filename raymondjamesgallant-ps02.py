a97d67ca1564f84f8c353161ab7911d1556df024
RaymondJGallant <111302502+RaymondJGallant@users.noreply.github.com>
2023-09-21 21:38:04 -0600

    Add files via upload
    This is my Python code for the assignment PS-02, it is a simple program that asks for users input (top three favorite movies) then returns back the movies on separate lines via a string.

---
+++ raymondjamesgallant-ps02.py
@@ -0,0 +1,14 @@
+# Author ="RaymondJamesGallant"
+# Time and Date = "9/18/2023 12:01"
+# Assignment Title = "A program that prints the users 3 favorite cinema titles on 3 seperate lines"
+# user input ="Favorite cinema"
+# output =  
+
+print('Please tell me your favorite movies')
+
+favorite_cinema_1 = input('Favorite Cinema 1: ')     
+favorite_cinema_2 = input('Favorite Cinema 2: ')
+favorite_cinema_3 = input('Favorite Cinema 3: ')
+
+print(favorite_cinema_1,'\n',favorite_cinema_2,'\n',favorite_cinema_3,'\n')
+
